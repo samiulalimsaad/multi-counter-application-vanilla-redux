f7c4745a76f84642ca0d5122a477531aed5b8b6d
samiulalimsaad <saadnbiuraj@gmail.com>
2022-08-14 23:43:33 +0600

    reducers added

--- store.js
+++ store.js
@@ -7,3 +7,34 @@ const RESET = "reset";
 const initialState = {
     value1: 0,
 };
+
+
+// reducer for different actions
+const counterReducer = (state = initialState, action) => {
+    if (action?.payload?.name && state[action?.payload?.name] === undefined) {
+        state[action?.payload?.name] = 0;
+    }
+    switch (action.type) {
+        case INCREMENT:
+            return {
+                ...state,
+                [action.payload.name]:
+                    state[action.payload.name] + action.payload.value,
+            };
+
+        case DECREMENT:
+            return {
+                ...state,
+                [action.payload.name]:
+                    state[action.payload.name] - action.payload.value,
+            };
+
+        case RESET:
+            const newState = {};
+            Object.keys(state).map((c) => (newState[c] = 0));
+            return newState;
+
+        default:
+            return state;
+    }
+};
